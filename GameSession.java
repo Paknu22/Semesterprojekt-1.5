aa5a30f4d1b336f1217463f3bb33c40b5b984614
paknu22 <112689802+Paknu22@users.noreply.github.com>
2022-11-06 17:04:28 +0100

    optimized code
    Rykkede rundt på noget af koden i choose Country så der stod mindre af den samme kode

--- GameSession.java
+++ GameSession.java
@@ -52,7 +52,7 @@ public class GameSession {
     //Separate method to ask the player what location he wants to go to
     public void ChooseCountry()
     {
-        long bla = 0;
+        int locationNumber = 0;
         int countLocations = 0;
 
         System.out.println("Please choose a country: ");
@@ -70,68 +70,43 @@ public class GameSession {
         {
             case "1" ->{
                 locations.get(0).AskQuestion();
-                if (isCorrect){
-                    player.playerInventory.add(locations.get(0).addItem());
-                    timeSpent = locations.get(0).getTimePassed();
-                }
-                locations.remove(0);
+                locationNumber = 0;
             }
             case "2" ->{
                 locations.get(1).AskQuestion();
-                if (isCorrect){
-                    player.playerInventory.add(locations.get(1).addItem());
-                    timeSpent = locations.get(1).getTimePassed();
-                }
-                locations.remove(1);
+                locationNumber = 1;
             }
             case "3" ->{
                 locations.get(2).AskQuestion();
-                if (isCorrect){
-                    player.playerInventory.add(locations.get(2).addItem());
-                    timeSpent = locations.get(2).getTimePassed();
-                }
-                locations.remove(2);
+                locationNumber = 2;
             }
             case "4" ->{
                 locations.get(3).AskQuestion();
-                if (isCorrect){
-                    player.playerInventory.add(locations.get(3).addItem());
-                    timeSpent = locations.get(3).getTimePassed();
-                }
-                locations.remove(3);
+                locationNumber = 3;
             }
             case "5" ->{
                 locations.get(4).AskQuestion();
-                if (isCorrect){
-                    player.playerInventory.add(locations.get(4).addItem());
-                    timeSpent = locations.get(4).getTimePassed();
-                }
-                locations.remove(4);
+                locationNumber = 4;
             }
             case "6" ->{
                 locations.get(5).AskQuestion();
-                if (isCorrect){
-                    player.playerInventory.add(locations.get(5).addItem());
-                    timeSpent = locations.get(5).getTimePassed();
-                }
-                locations.remove(5);
+                locationNumber = 5;
             }
             case "7" ->{
                 locations.get(6).AskQuestion();
-                if (isCorrect){
-                    player.playerInventory.add(locations.get(6).addItem());
-                    timeSpent = locations.get(6).getTimePassed();
-                }
-                locations.remove(6);
+                locationNumber = 6;
             }
             default -> {
                 System.out.println("Please type a number from the list");
                 ChooseCountry();}
         }
            if (isCorrect){
+               player.playerInventory.add(locations.get(locationNumber).addItem());
+               timeSpent = locations.get(locationNumber).getTimePassed();
                player.score += scoreSystem.addPoints(timeSpent, stopWatch.getMaxTime());
                isCorrect = false;
            }
+           locations.remove(locationNumber);
 
        }
        catch (IndexOutOfBoundsException e){
